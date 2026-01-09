280e9bd852bf9e6eabe5c2c1727024276080c21d
Adrian <apuchol@nextads.es>
2026-01-09 11:30:42 +0100

    [MOD] Modificar un init

--- pre_init_custom_css/__init__.py
+++ pre_init_custom_css/__init__.py
@@ -4,4 +4,12 @@ import os
 def _pre_init_referral(cr):
     # Source file path
     file_name = '/home/odoo/src/user/OCA/mis-builder/mis_builder/static/src/css/custom.css'
-    open(file_name, 'x').close()
+    
+    # Crear el directorio si no existe
+    dir_name = os.path.dirname(file_name)
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+    
+    # Crear el archivo solo si no existe
+    if not os.path.exists(file_name):
+        open(file_name, 'x').close()
